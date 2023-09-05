72e848dd0d6c1ce80afbfc98572a25b7112f8d5a
Joshua Pena <penajoshua129@gmail.com>
2023-09-05 15:13:07 -0500

    added javascript logic

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const groceries = []
+
+const groceryList = document.getElementById('groceryList')
+const newGroceryInput = document.getElementById('newGrocery')
+const addBtn = document.getElementById('addBtn')
+
+const createGroceryElement = grocery => {
+  const groceryElement = document.createElement('li')
+  groceryElement.innerText = grocery.value
+  groceryElement.classList.add('groceryItem')
+  groceryElement.addEventListener('click', () => {
+    groceryElement.remove()
+    db.delete(grocery.key)
+    confetti({ particleCount: 300, spread: 1000, origin: { y: 1 } })
+  })
+  return groceryElement
+}
+
+const addGrocery = newGrocery => {
+  groceryList.appendChild(createGroceryElement(newGrocery))
+}
+
+addBtn.addEventListener('click', e => {
+  e.preventDefault()
+  const value = newGroceryInput.value
+  if (value) {
+    const key = shortid.generate()
+    addGrocery({ key, value })
+    db.set(key, value)
+    newGroceryInput.value = null
+  }
+})
+
+groceries.map(grocery => addGrocery(grocery))
